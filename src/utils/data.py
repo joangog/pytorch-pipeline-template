45425583bc283c64a2b5ccacd50eca784f0abf71
Ioanna <i.gogou@amsterdamumc.nl>
2025-09-11 12:04:20 +0200

    Added dataset splitting for cross validation

--- src/utils/data.py
+++ src/utils/data.py
@@ -14,7 +14,43 @@ def select_dataset(name, args):
         raise ValueError('Unknown dataset')
 
 
-def split_dataset(dataset, val_ratio, test_ratio, split_type=None, seed=42):
+def split_dataset_in_two_sets(dataset, ratio, split_type, seed):
+    """
+    Splits the dataset into two sets.
+    :param dataset: Dataset object to split.
+    :param ratio: Ratio of dataset to use for the second set.
+    :param split_type: Type of splitting strategy to use.
+                        If 'random', we use random samples for each split.
+                        If 'stratify', we take use class-balanced splits.
+                        If 'consecutive', we take consecutive samples for each split.
+    :param seed: Random seed for splitting.
+    :return: The Subset objects for the two sets.
+    """
+    n_second = int(ratio * len(dataset))
+    n_first = len(dataset) - n_second
+
+    if split_type == 'random':
+        first_idx = np.random.choice(range(len(dataset)), n_first, replace=False)
+        second_idx = list(set(range(len(dataset))) - set(first_idx))
+    elif split_type == 'stratify':
+        labels = [dataset.labels[image] for image in dataset.images]
+        first_idx, second_idx = train_test_split(range(len(dataset)), test_size=ratio,
+                                                 stratify=labels,
+                                                 random_state=seed)
+    elif split_type == 'consecutive':
+        first_idx = list(range(n_first))
+        second_idx = list(range(n_first, n_first + n_second))
+    else:
+        raise ValueError('Invalid split type. Please choose from "random", "stratify", or "consecutive"')
+
+    first_set = Subset(dataset, first_idx)
+    second_set = Subset(dataset, second_idx)
+
+    return first_set, second_set
+
+
+def split_dataset(dataset, val_ratio, test_ratio, split_type='consecutive', seed=42, train_val_folds=1, train_idx=None,
+                  val_idx=None, test_idx=None):
     """
     Splits the dataset into training and test sets.
     :param dataset: Dataset object to split.
@@ -23,38 +59,44 @@ def split_dataset(dataset, val_ratio, test_ratio, split_type=None, seed=42):
     :param split_type: Type of splitting strategy to use.
                         If 'random', we use random samples for each split.
                         If 'stratify', we take use class-balanced splits.
-                        If None or '', we take consecutive samples for each split.
+                        If 'consecutive', we take consecutive samples for each split.
+                        If 'idx', we take samples based on given ids.
     :param seed: Random seed for splitting.
+    :param train_val_folds: Number of folds for cross-validation on the train+val set. If 1, no cross-validation is performed.
+    :param train_idx: List or (Dictionary of Lists, when multiple folds) of indices for training set (to be used with split_type='ids').
+    :param val_idx: List or (Dictionary of Lists, when multiple folds) of indices for validation set (to be used with split_type='ids').
+    :param test_idx: List of indices for test set (to be used with split_type='ids').
     :return: The Subset objects for the training and test sets.
     """
-    n_test = int(test_ratio * len(dataset))
-    n_val = int(val_ratio * len(dataset))
-    n_train = len(dataset) - n_test - n_val
 
-    if split_type == 'random':  # If we want random splits
-        train_idx = np.random.choice(range(len(dataset)), n_train, replace=False)
-        val_idx = np.random.choice(list(set(range(len(dataset))) - set(train_idx)), n_val, replace=False)
-        test_idx = list(set(range(len(dataset))) - set(train_idx) - set(val_idx))
-    elif split_type == 'stratify':  # If we want class-balanced splits
-        labels = [dataset.labels[image] for image in dataset.images]
-        train_idx, val_test_idx = train_test_split(range(len(dataset)), test_size=(n_val + n_test),
-                                                   stratify=labels,
-                                                   random_state=seed)  # Split dataset into train and val+test
-        val_idx, test_idx = train_test_split(val_test_idx, test_size=test_ratio,
-                                             stratify=[labels[i] for i in val_test_idx],
-                                             random_state=seed)  # Split val+test into val and test
-    elif split_type is None or split_type == '':
-        train_idx = list(range(n_train))
-        val_idx = list(range(n_train, n_train + n_val))
-        test_idx = list(set(range(len(dataset))) - set(train_idx) - set(val_idx))
+    # Initialize
+    train_set = {}  # Dictionary (for every fold)
+    val_set = {}
+
+    train_val_set, test_set = split_dataset_in_two_sets(dataset, test_ratio, split_type, seed)
+
+    if split_type != 'idx':  # If not split from ids, then split using the given strategy
+        if train_val_folds > 1:
+            for fold in range(train_val_folds):
+                train_set[fold], val_set[fold] = split_dataset_in_two_sets(train_val_set, val_ratio / (1 - test_ratio),
+                                                                           split_type, seed)
+        else:
+            train_set, val_set = split_dataset_in_two_sets(train_val_set, val_ratio / (1 - test_ratio), split_type,
+                                                           seed)
+    else:  # If split from ids
+        if train_val_folds > 1:
+            for fold in range(train_val_folds):
+                if train_idx is None or val_idx is None or test_idx is None:
+                    raise ValueError('When using split_type="ids", train_ids, val_ids and test_ids must be provided.')
+                train_set[fold] = Subset(dataset, train_idx[fold])
+                val_set[fold] = Subset(dataset, val_idx[fold])
+            test_set = Subset(dataset, test_idx)
+
+    # Assert whole dataset is used
+    if train_val_folds == 1:
+        assert len(train_set) + len(val_set) + len(test_set) == len(dataset)
     else:
-        raise ValueError('Invalid split type. Please choose from "random", "stratify", or "None"')
-
-    # TODO: implement splitting for cross-validation (you might use scikit.kfold but think about how to make stratified ones)
-
-    train_set = Subset(dataset, train_idx)
-    val_set = Subset(dataset, val_idx)
-    test_set = Subset(dataset, test_idx)
+        assert len(train_set[0]) + len(val_set[0]) + len(test_set) == len(dataset)
 
     return train_set, val_set, test_set
 
